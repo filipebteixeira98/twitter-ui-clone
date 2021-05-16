c5bc48e859dd2551752f49390ccd95400a44c10b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-16 00:18:35 -0300

    chore: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <GlobalStyles />
+    </>
   );
 }
 
